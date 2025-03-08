7134d6b212e374cc8df2c537fdc49e0c686a9f8a
Kulum-eng <120421270+Kulum-eng@users.noreply.github.com>
2025-03-08 14:51:56 -0600

    jijuienvios

--- consumerEnvios/consumer.js
+++ consumerEnvios/consumer.js
@@ -1,31 +1,55 @@
 const amqplib = require("amqplib/callback_api");
 
 const urlServerRabbitMQ = "amqp://ale:ale123@ec2-3-83-91-51.compute-1.amazonaws.com/";
+const endpoint = "http://localhost:8080/shipments/";
+
+
 const queue = "envios";
 
-function sendMessageToQueue(message) {
-  amqplib.connect(urlServerRabbitMQ, function (error0, connection) {
-    if (error0) {
-      throw error0;
+amqplib.connect(urlServerRabbitMQ, function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
     }
 
-    connection.createChannel(function (error1, channel) {
-      if (error1) {
-        throw error1;
-      }
-
-      channel.assertQueue(queue, {
-        durable: true,
-      });
-
-      channel.sendToQueue(queue, Buffer.from(message));
-      console.log(" [x] Sent %s", message);
-
-      setTimeout(() => {
-        connection.close();
-      }, 500);
+    channel.assertQueue(queue, {
+      durable: true,
     });
-  });
-}
 
-sendMessageToQueue(message);
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+    
+    channel.consume(
+      queue,
+      async function (msg) {
+        console.log(" [x] Received %s", msg.content.toString());
+         try {
+           const headers = {
+             "Content-Type": "application/json",
+           };
+           const req = {
+            method: "POST",
+             body: msg.content.toString(),
+             headers,
+           };
+           const result = await fetch(
+             endpoint,
+             req
+           );
+    
+           const data = await result.json();
+           console.log(data);
+           console.log(JSON.parse(msg.content.toString()));
+         } catch (error) {
+           console.log(error);
+         }
+      },
+      {
+        noAck: true,
+      }
+    );
+  });
+});
