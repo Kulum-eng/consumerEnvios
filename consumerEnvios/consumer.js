abc3d3f24ecb8d537a7a6fef3e014bd0164db945
Kulum-eng <120421270+Kulum-eng@users.noreply.github.com>
2025-03-08 19:18:14 -0600

    ya

--- consumerEnvios/consumer.js
+++ consumerEnvios/consumer.js
@@ -1,8 +1,7 @@
 const amqplib = require("amqplib/callback_api");
 
 const urlServerRabbitMQ = "amqp://ale:ale123@ec2-3-83-91-51.compute-1.amazonaws.com/";
-const endpoint = "http://localhost:8080/shipments/";
-
+const endpoint = "http://localhost:8086/shipments/";  
 
 const queue = "envios";
 
@@ -26,26 +25,23 @@ amqplib.connect(urlServerRabbitMQ, function (error0, connection) {
       queue,
       async function (msg) {
         console.log(" [x] Received %s", msg.content.toString());
-         try {
-           const headers = {
-             "Content-Type": "application/json",
-           };
-           const req = {
+        try {
+          const headers = {
+            "Content-Type": "application/json",
+          };
+          const req = {
             method: "POST",
-             body: msg.content.toString(),
-             headers,
-           };
-           const result = await fetch(
-             endpoint,
-             req
-           );
+            body: msg.content.toString(),
+            headers,
+          };
+          const result = await fetch(endpoint, req);
     
-           const data = await result.json();
-           console.log(data);
-           console.log(JSON.parse(msg.content.toString()));
-         } catch (error) {
-           console.log(error);
-         }
+          const data = await result.json();
+          console.log(data);
+          console.log(JSON.parse(msg.content.toString()));
+        } catch (error) {
+          console.log(error);
+        }
       },
       {
         noAck: true,
